d2a3ad09b9e3d44c574b0e40003d3678c569390c
valshaden <valshaden@yandex.ru>
2025-08-25 11:33:31 +0300

    +

---
+++ valshaden/m7-1-api/01.py
@@ -0,0 +1,14 @@
+
+from langchain_gigachat.chat_models import GigaChat
+
+'''
+pip install langchain>=0.3.25
+pip install langchain-gigachat>=0.3.10
+'''
+
+model = GigaChat(
+    model="GigaChat-2-Max",
+    verify_ssl_certs=False,
+    credentials="MjY5NzhmNDItYjU4Ny00Y2ZlLTgzOTUtMjIyYzA3NzQzZGYwOjZkOTYyOThkLTE5YzEtNGVhNC05ZmEyLTA1ZTkxZGE5OTE1Mg=="
+)
+
