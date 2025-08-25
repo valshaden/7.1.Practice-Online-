99be655ad4f2f572f64e3f2add9603b5e160cb80
valshaden <valshaden@yandex.ru>
2025-08-25 11:55:39 +0300

    + 04

---
+++ 03.py
@@ -0,0 +1,38 @@
+
+from langchain_gigachat.chat_models import GigaChat
+
+'''
+pip install langchain>=0.3.25
+pip install langchain-gigachat>=0.3.10
+'''
+
+model = GigaChat(
+    model="GigaChat-2-Max",
+    verify_ssl_certs=False,
+    credentials="MjY5NzhmNDItYjU4Ny00Y2ZlLTgzOTUtMjIyYzA3NzQzZGYwOjZkOTYyOThkLTE5YzEtNGVhNC05ZmEyLTA1ZTkxZGE5OTE1Mg=="
+)
+
+#message = model.invoke("Привет, как дела?")
+
+#message = {"role": "user", "content": "Привет, как дела?"}
+#response = model.invoke([message])
+#print(response.content)
+
+#message = {"role": "assistant", "content": response.content}
+
+#message = {"role": "user", "content": "Прогноз погоды в Москве на 10 дней."}
+#response = model.invoke([message])
+#print(response.content)
+
+message = {
+    "role": "user",
+    "content": "Распознай текст с этого изображения и выведи его полностью.",
+    "attachments": ["Image20250825114326.png"]
+}
+
+with open("Image20250825114326.png", "rb") as image_file:
+    file_uploaded_id = model.upload_file(image_file).id_
+    
+response = model.invoke([message])
+print(response.content)
+# не работает
